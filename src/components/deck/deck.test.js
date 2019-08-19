b3c05ad1466c7ed5e8009b10e66642af8c182a87
Osama Khalid <osama@codinghazard.com>
2019-08-19 15:49:45 +0400

    adding tests

--- src/components/deck/deck.test.js
+++ src/components/deck/deck.test.js
@@ -7,3 +7,9 @@ it('renders without crashing', () => {
   ReactDOM.render(<Deck suit="" cards={[]} phase="play" round={0} current={false} active={false} onCardSelect={() => {}} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders active card correctly', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Deck suit="" cards={["T3", "S2"]} phase="play" round={0} current={true} active={true} onCardSelect={() => {}} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
--- src/server.js
+++ src/server.js
@@ -132,3 +132,8 @@ app.use(router.routes()).use(router.allowedMethods());
 app.listen(API_PORT, () => {
   console.log(`API serving at: http://localhost:${API_PORT}/`);
 });
+
+export {
+  app,
+  server
+}
