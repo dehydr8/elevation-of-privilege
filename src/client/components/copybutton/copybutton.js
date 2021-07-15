fcccf813bdb429b88e3e34e5b0d79c2a3785df89
Matt Jones <mej34@cam.ac.uk>
2021-07-15 14:04:27 +0200

    Added tick and fixed 'undefined' being copied

--- src/client/components/copybutton/copybutton.js
+++ src/client/components/copybutton/copybutton.js
@@ -1,36 +1,58 @@
 import React from 'react';
 import {Button} from 'reactstrap';
 import { copyToClipboard } from '../../../utils/utils';
+import { faCopy, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 
 class CopyButton extends React.Component {
     static propTypes = {
-        color: PropTypes.string.isRequired
+        text: PropTypes.string.isRequired,
+        color: PropTypes.string,
+        active: PropTypes.bool,
+        block: PropTypes.bool,
+        disabled: PropTypes.bool,
+        outline: PropTypes.bool,
+        size: PropTypes.string
     }
 
     constructor(props) {
         super(props);
-        this.state = {color: this.props.color};
+        this.state = {
+            color: this.props.color,
+            copied: false
+        };
+
+        this.handleClick = this.handleClick.bind(this);
     }
 
     render() {
         return (
-            <Button color={this.state.color} onClick={() => this.handleClick()}>
-                Copy
+            <Button 
+                color={this.state.color}
+                onClick={this.handleClick}
+                active={this.props.active}
+                block={this.props.block}
+                disabled={this.props.disabled}
+                size={this.props.size}
+            >
+                <FontAwesomeIcon icon={this.state.copied ? faCheck : faCopy} />
+                {' '}
+                {this.props.children}
             </Button>
         )
     }
 
     handleClick() {
-        copyToClipboard(this.state.text).then(() => {
-            this.setState({color: 'success'});
+        copyToClipboard(this.props.text).then(() => {
+            this.setState({color: 'success', copied: true});
             
         }, () => {
             //If the copy fails, maybe alert the user somehow
             this.setState({color: 'danger'})
         });
         setTimeout(() => {
-            this.setState({color: this.props.color})
+            this.setState({color: this.props.color, copied: false})
         }, 500);
     }
 }
