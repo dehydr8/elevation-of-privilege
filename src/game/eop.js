707a1ad57197436a42baa5bceb25c856cc15a4f1
Matt Jones <matthew.jones@tngtech.com>
2021-07-19 12:03:36 +0200

    update syntax of game definition

--- src/game/eop.js
+++ src/game/eop.js
@@ -127,86 +127,88 @@ export const ElevationOfPrivilege = {
     draw
   },
   playerView: PlayerView.STRIP_SECRETS,
-  flow: {
-    startingPhase: 'play',
-    endGameIf: (G, ctx) => {
-      if (G.round > G.maxRounds) {
-        let scores = [...G.scores];
-        let winner = 0, max = -1;
-        for (let i=0; i<scores.length; i++) {
-          if (scores[i] > max) {
-            winner = i;
-            max = scores[i];
-          }
+  endIf: (G, ctx) => {
+    if (G.round > G.maxRounds) {
+      let scores = [...G.scores];
+      let winner = 0, max = -1;
+      for (let i=0; i<scores.length; i++) {
+        if (scores[i] > max) {
+          winner = i;
+          max = scores[i];
         }
-        return winner;
       }
-    },
-    phases: {
-      threats: {
-        turnOrder: {
+      return winner;
+    }
+  },
+  phases: {
+    threats: {
+      turn: {
+        Order: {
           first: function first(G, ctx) {
             return ctx.playOrderPos;
           },
           next: function next(G, ctx) {
             return ctx.playOrderPos;
           },
-          actionPlayers: {
+          activePlayers: {
             value: (G, ctx) => getPlayers(ctx.numPlayers), // hack, ANY wasn't working because of int actionPlayers
           }
         },
-        next: 'play',
-        allowedMoves: ['addOrUpdateThreat', 'deleteThreat', 'pass', 'selectDiagram', 'selectComponent', 'selectThreat', 'toggleModal', 'toggleModalUpdate', 'updateThreat'],
-        endPhaseIf: (G, ctx) => {
-          let passed = [...G.passed];
-          return passed.length >= ctx.numPlayers;
-        },
-        onPhaseEnd: (G, ctx) => {
-          let dealt = [...G.dealt];
-          let order = [...G.order];
-          let suit = G.suit;
-          let dealtBy = G.dealtBy;
-          let scores = [...G.scores];
-          let round = G.round;
-          let lastWinner = G.lastWinner;
-
-          // calculate the scores
-          if (dealt.length >= ctx.numPlayers) {
-            let idx = getWinner(suit, dealt);
-            lastWinner = order[idx];
-    
-            scores[lastWinner]++;
-    
-            order = [];
-            for (let i=0; i<ctx.numPlayers; i++) {
-              order.push(i);
-            }
-            for (let i=0; i<lastWinner; i++) {
-              let element = order.shift();
-              order.push(element);
-            }
-    
-            dealt = [];
-            suit = "";
-            dealtBy = "";
-            round++;
-          }
-
-          return {
-            ...G,
-            dealt,
-            lastWinner,
-            dealtBy,
-            order,
-            suit,
-            scores,
-            round,
-            passed: [], // reset the passed array when the phase ends
-          }
-        },
       },
-      play: {
-        turnOrder: {
+      next: 'play',
+      allowedMoves: ['addOrUpdateThreat', 'deleteThreat', 'pass', 'selectDiagram', 'selectComponent', 'selectThreat', 'toggleModal', 'toggleModalUpdate', 'updateThreat'],
+      endIf: (G, ctx) => {
+        let passed = [...G.passed];
+        return passed.length >= ctx.numPlayers;
+      },
+      onEnd: (G, ctx) => {
+        let dealt = [...G.dealt];
+        let order = [...G.order];
+        let suit = G.suit;
+        let dealtBy = G.dealtBy;
+        let scores = [...G.scores];
+        let round = G.round;
+        let lastWinner = G.lastWinner;
+
+        // calculate the scores
+        if (dealt.length >= ctx.numPlayers) {
+          let idx = getWinner(suit, dealt);
+          lastWinner = order[idx];
+  
+          scores[lastWinner]++;
+  
+          order = [];
+          for (let i=0; i<ctx.numPlayers; i++) {
+            order.push(i);
+          }
+          for (let i=0; i<lastWinner; i++) {
+            let element = order.shift();
+            order.push(element);
+          }
+  
+          dealt = [];
+          suit = "";
+          dealtBy = "";
+          round++;
+        }
+
+        return {
+          ...G,
+          dealt,
+          lastWinner,
+          dealtBy,
+          order,
+          suit,
+          scores,
+          round,
+          passed: [], // reset the passed array when the phase ends
+        }
+      },
+    },
+    play: {
+      start: true,
+      turn: {
+        Order: {
           playOrder: function playOrder(G) {
             return G.order;
           },
@@ -217,9 +219,7 @@ export const ElevationOfPrivilege = {
             return (ctx.playOrderPos + 1) % ctx.playOrder.length;
           }
         },
-        next: 'threats',
-        allowedMoves: ['draw', 'selectDiagram', 'selectComponent', 'selectThreat'],
-        onTurnEnd: (G, ctx) => {
+        onEnd: (G, ctx) => {
           ctx.events.endPhase();
           return {
             ...G,
@@ -227,8 +227,10 @@ export const ElevationOfPrivilege = {
           }
         },
       },
+      next: 'threats',
+      allowedMoves: ['draw', 'selectDiagram', 'selectComponent', 'selectThreat'],
     },
-  }
+  },
 };
 
 function toggleModal(G, ctx) {
