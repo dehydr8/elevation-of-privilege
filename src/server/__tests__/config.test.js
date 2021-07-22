9048e2ac0405ee0df2f65c4a2340cba96ef1acb5
Matt Jones <matthew.jones@tngtech.com>
2021-07-22 17:11:59 +0200

    remove mongo

--- src/server/__tests__/config.test.js
+++ src/server/__tests__/config.test.js
@@ -1,14 +1,6 @@
 import { getDatabase } from '../config';
 import { FlatFile } from 'boardgame.io/server';
 
-// it('returns correct Mongo database', () => {
-//   process.env = {
-//     DATA_STORE: "mongo",
-//   };
-//   const db = getDatabase();
-//   expect(db).toBeInstanceOf(Mongo);
-// });
-
 it('returns correct FlatFile database', () => {
   process.env = {
     DATA_STORE: "whatever",
--- src/server/config.js
+++ src/server/config.js
@@ -1,12 +1,6 @@
 import { FlatFile } from 'boardgame.io/server';
 
 export function getDatabase() {
-  // if (process.env.DATA_STORE === 'mongo') {
-  //   return new Mongo({
-  //     url: process.env.MONGO_URI,
-  //     dbname: process.env.MONGO_DATABASE,
-  //   });
-  // } else {
     return new FlatFile({
       dir: 'db',
       logging: false,
