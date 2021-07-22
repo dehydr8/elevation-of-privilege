d4b4e7add6b65a0bf39774907753d427a5ce7835
Matt Jones <mej34@cam.ac.uk>
2021-07-22 13:03:34 +0200

    Allow 2-player game

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,7 +15,7 @@ export const STARTING_CARD_MAP = {
     'E': 'E5'
   }
 
-export const MIN_NUMBER_PLAYERS = 3;
+export const MIN_NUMBER_PLAYERS = 2;
 export const MAX_NUMBER_PLAYERS = 9;
 
 export const SERVER_PORT = process.env.SERVER_PORT || 8000;
