4b0ebd2ec96b31c37069274bcc7a53a27ddae6be
Matt Jones <matthew.jones@tngtech.com>
2021-08-18 10:20:10 +0200

    better error handling amd fix auth

--- src/client/components/downloadbutton/downloadbutton.js
+++ src/client/components/downloadbutton/downloadbutton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from 'reactstrap';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 import { API_PORT } from '../../../utils/constants';
@@ -24,6 +24,11 @@ class DownloadButton extends React.Component {
     this.apiBase = (process.env.NODE_ENV === 'production') ? '/api' : `${window.location.protocol}//${window.location.hostname}:${API_PORT}`;
     this.apiEndpointUrl = this.apiEndpointUrl.bind(this);
     this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+        color: this.props.color,
+        icon: faDownload,
+    }
   }
 
   apiEndpointUrl() {
@@ -38,7 +43,7 @@ class DownloadButton extends React.Component {
   auth() {
     const user = this.props.playerID;
     const pass = this.props.secret;
-    return {'Authorization': 'Basic ' + (user + ":" + pass).toString('base64')}
+    return {'Authorization': 'Basic ' + Buffer.from(user + ':' + pass).toString('base64')};
   }
 
   async handleClick() {
@@ -60,7 +65,10 @@ class DownloadButton extends React.Component {
       a.click();
       a.remove();
     } catch(err) {
-      // TODO: Create an alert or sth when this fails
+      this.setState({icon: faTimes, color: 'danger'})
+      setTimeout(() => {
+        this.setState({icon: faDownload, color: this.props.color});
+      }, 500)
       console.error(err);
       return;
     }
@@ -69,15 +77,16 @@ class DownloadButton extends React.Component {
   render() {
     return (
       <Button
-        color={this.props.color}
+        color={this.state.color}
         onClick={this.handleClick}
         active={this.props.active}
         block={this.props.block}
         disabled={this.props.disabled}
         size={this.props.size}
       >
-        <FontAwesomeIcon icon={faDownload} /> &nbsp;
-        {this.props.children}
+        <FontAwesomeIcon icon={this.state.icon} fixedWidth /> &nbsp;
+        {this.props.children }
+        {this.state.text}
       </Button>
     );
   }
