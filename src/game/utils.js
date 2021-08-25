a3470a4bfd9c69ba2ba052c7327957507ea2ecfb
Matt Jones <matthew.jones@tngtech.com>
2021-08-25 18:03:47 +0200

    add modeltype to G

--- src/game/utils.js
+++ src/game/utils.js
@@ -1,4 +1,4 @@
-import { DEFAULT_START_SUIT, DEFAULT_TURN_DURATION, STARTING_CARD_MAP, CARD_LIMIT, DECK_HANDS, DECK_SUITS, INVALID_CARDS, TRUMP_CARD_PREFIX, DEFAULT_GAME_MODE } from '../utils/constants';
+import { DEFAULT_START_SUIT, DEFAULT_TURN_DURATION, STARTING_CARD_MAP, CARD_LIMIT, DECK_HANDS, DECK_SUITS, INVALID_CARDS, TRUMP_CARD_PREFIX, DEFAULT_GAME_MODE, MODEL_TYPE_DEFAULT } from '../utils/constants';
 import { isGameModeCornucopia } from '../utils/utils';
 
 
@@ -54,6 +54,7 @@ export function shuffleCards(ctx, startingCard) {
 export function setupGame(ctx, setupData) {
   const startSuit = (setupData) ? setupData.startSuit || DEFAULT_START_SUIT : DEFAULT_START_SUIT;
   const gameMode = setupData ? setupData.gameMode || DEFAULT_GAME_MODE : DEFAULT_GAME_MODE;
+  const modelType = setupData ? setupData.modelType || MODEL_TYPE_DEFAULT : MODEL_TYPE_DEFAULT;
   const turnDuration = (setupData) ?
     setupData.turnDuration || (setupData.turnDuration !== 0 && DEFAULT_TURN_DURATION) :
     DEFAULT_TURN_DURATION;
@@ -86,6 +87,7 @@ export function setupGame(ctx, setupData) {
     startingCard: startingCard,
     gameMode: gameMode,
     turnDuration: turnDuration,
+    modelType,
   }
   return ret;
 }
