9f3cee5479e9dea72e7d41a7c58673d7b9a0986e
Johannes Loher <johannes.loher@tngtech.com>
2023-03-24 16:35:52 +0100

    Convert Timer component to TypeScript
    Signed-off-by: Johannes Loher <johannes.loher@tngtech.com>

---
+++ src/client/components/timer/timer.test.tsx
@@ -0,0 +1,36 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import Timer from './timer';
+import { render, screen } from '@testing-library/react';
+
+describe('Timer', () => {
+  it('renders without crashing', async () => {
+    render(<Timer duration={60} targetTime={Date.now() + 60 * 1000} />);
+
+    const element = await screen.findByTestId('timer-wrapper');
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it("doesn't render when not active", () => {
+    render(
+      <Timer
+        active={false}
+        duration={60}
+        targetTime={Date.now() + 60 * 1000}
+      />,
+    );
+
+    const element = screen.queryByTestId('timer-wrapper');
+
+    expect(element).not.toBeInTheDocument();
+  });
+
+  it("doesn't render when duration is zero", () => {
+    render(<Timer duration={0} targetTime={Date.now() + 60 * 1000} />);
+
+    const element = screen.queryByTestId('timer-wrapper');
+
+    expect(element).not.toBeInTheDocument();
+  });
+});
---
+++ src/client/components/timer/timer.tsx
@@ -0,0 +1,65 @@
+import React, { FC, useEffect, useState } from 'react';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+
+import './timer.css';
+
+const renderTime: FC<{ remainingTime: number }> = ({ remainingTime }) => {
+  return (
+    <div className="timer">
+      <div className="value">
+        {Math.floor(remainingTime / 60)}:
+        {(remainingTime % 60).toString().padStart(2, '0')}
+      </div>
+    </div>
+  );
+};
+
+type TimerProps = {
+  active?: boolean;
+  duration: number;
+  targetTime: number;
+};
+
+const Timer: FC<TimerProps> = ({ active = true, duration, targetTime }) => {
+  const [painted, setPainted] = useState(false);
+
+  useEffect(() => {
+    // This is a bit of a workaround
+    // The timer is updated with window.requestAnimationFrame however the page can be rendered before
+    // this is called if it is not the current tab. This means the remaining time will be set to the
+    // duration but doesn't start counting down until the tab is opened
+    // By updating the component when requestAnimationFrame is first called, the initial remaining
+    // time is reset and the timer shows the correct time
+    const animationFrame = window.requestAnimationFrame(() => {
+      if (!painted) {
+        setPainted(true);
+      }
+    });
+
+    return () => window.cancelAnimationFrame(animationFrame);
+  }, [painted]);
+
+  const timeDifferenceInSeconds = Math.floor((targetTime - Date.now()) / 1000);
+
+  return active && duration > 0 ? (
+    <div className="timer-wrapper" data-testid="timer-wrapper">
+      <CountdownCircleTimer
+        key={`timer-${active}-${painted}`}
+        isPlaying
+        size={150}
+        duration={duration}
+        initialRemainingTime={Math.max(timeDifferenceInSeconds, 0)}
+        colors={[
+          ['#28a745', 0.625],
+          ['#ffc107', 0.25],
+          ['#dc3545', 0.125],
+        ]}
+        onComplete={() => [false, 0]}
+      >
+        {renderTime}
+      </CountdownCircleTimer>
+    </div>
+  ) : null;
+};
+
+export default Timer;
