52c680128a89aeb4a1cf00466ac9a74ea493f0de
Johannes Loher <johannes.loher@tngtech.com>
2023-04-14 15:06:58 +0200

    Convert Footer to TypeScript
    Signed-off-by: Johannes Loher <johannes.loher@tngtech.com>

---
+++ src/client/components/footer/footer.test.tsx
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Footer from './footer';
+
+describe('Footer', () => {
+  it('renders without crashing', async () => {
+    // given
+    render(<Footer />);
+
+    // when
+    const links = await screen.findAllByRole('link', {
+      name: 'TNG Technology Consulting',
+    });
+
+    // then
+    expect(links.length).toBe(2);
+  });
+});
---
+++ src/client/components/footer/footer.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import type { FC } from 'react';
+import packageJson from '../../../../package.json';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+type FooterProps = {
+  short?: boolean;
+};
+
+const Footer: FC<FooterProps> = ({ short }) => (
+  <small className="text-muted">
+    v{packageJson.version}
+    {!short && (
+      <span>
+        {' '}
+        - made with{' '}
+        {/* @ts-expect-error @fortawesome/react-fontawesome uses an older version of @fortawesome/fontawesome-svg-core (1.3.0), which makes the types incompatible. It still works correctly at runtime. */}
+        <FontAwesomeIcon icon={faHeart} style={{ color: '#00cc00' }} /> at
+        Careem and{' '}
+        <a href="https://www.tngtech.com/en/">TNG Technology Consulting</a> -
+        Elevation of Privilege was originally invented at Microsoft, Cornucopia
+        was developed at OWASP, Cumulus was started at{' '}
+        <a href="https://www.tngtech.com/en/">TNG Technology Consulting</a>.
+      </span>
+    )}
+  </small>
+);
+
+Footer.defaultProps = {
+  short: false,
+};
+
+export default Footer;
